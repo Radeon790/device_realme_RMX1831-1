78ad6351602e58ee3b9c721bb5d24316b3935288
SparXFusion <s2234nadar@gmail.com>
2021-05-22 15:14:55 +0530

    RMX1831: build libandroidicu
    fixes:
    05-22 09:14:29.061   701   701 E HAL     : dlopen failed: library "libandroidicu.so" not found: needed by /system/lib/libxml2.so in namespace (default)
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -176,6 +176,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Apex
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
 
 # Seccomp
 PRODUCT_COPY_FILES += \
